9774ca92d6dec64ee8a01598859d16119b783484
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-12-23 18:35:56 +0100

    Engage!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Download Texture Town textures
+
+https://textures.neocities.org/
+
+This repository also contains a snapshot of all textures.
+
+Check the website for more information and new textures.
+
+If you want to download all the textures run `python download.py`.
---
+++ download.py
@@ -0,0 +1,23 @@
+import os
+import json
+import urllib.request
+
+url = "https://textures.neocities.org/manifest.json"
+
+with urllib.request.urlopen(url) as response:
+    data = json.load(response)
+
+base_url = data["info"]["base_url"]
+texture_path = data["info"]["textures_folder"]
+
+os.makedirs("textures", exist_ok=True)
+
+for catalogue in data["catalogue"]:
+    os.makedirs(f"textures/{catalogue['name']}", exist_ok=True)
+
+    for file in catalogue["files"]:
+        print(f"- downloading {catalogue['name']}/{file}")
+        urllib.request.urlretrieve(f"{base_url}/{texture_path}/{catalogue['name']}/{file}", f"textures/{catalogue['name']}/{file}")
+
+with open("version.txt", "w", encoding="utf-8") as f:
+    f.write(data["info"]["version"])
